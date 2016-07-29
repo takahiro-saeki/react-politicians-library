880455e9f71c9a9d33e7df5a59c7125d4f528bf4
takahiro-saeki <hiroooon69@gmail.com>
2016-07-29 23:58:08 +0900

    add some library and modules

--- assets/js/container/Main.jsx
+++ assets/js/container/Main.jsx
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 import request from 'superagent';
+import uuid from 'node-uuid';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
+import Divider from 'material-ui/Divider';
 import Mui from '../data/mui';
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import style from '../../css/style.css';
 const url = {
   req: 'http://seiji.kpi-net.com/api/',
-  sample: 'http://seiji.kpi-net.com/api/?type=1&count=10&format=json'
+  sample: 'http://seiji.kpi-net.com/api/?type=1&count=1&format=json'
 }
 
 export default class Main extends Component {
@@ -44,18 +46,34 @@ export default class Main extends Component {
 
   render() {
     const list = this.state.body.map(body => {
-      return (<Card>
-        <CardHeader
-          title={body.name}
-          subtitle={body.yomi}
-          actAsExpander={true}
-          showExpandableButton={true}
-          />
-        <CardText expandable={true}>
-          <div></div>
-        </CardText>
-      </Card>
-    )
+      return (
+        <Card key={uuid.v4()}>
+          <CardHeader
+            title={body.name}
+            subtitle={body.yomi}
+            actAsExpander={true}
+            showExpandableButton={true}
+            />
+          <CardText expandable={true}>
+            <Divider />
+            <section style={{display: 'flex', padding: '1rem 0'}}>
+              <div style={{width: '30%'}}>地域</div>
+              <div style={{width: '70%'}}>{body.area}</div>
+            </section>
+            <Divider />
+            <section style={{display: 'flex', padding: '1rem 0'}}>
+              <div style={{width: '30%'}}>カテゴリー</div>
+              <div style={{width: '70%', borderLeft: '1px solid #CCC', padding: '0 0 0 .5rem'}}>{body.category}</div>
+            </section>
+            <Divider />
+            <section style={{display: 'flex', padding: '1rem 0'}}>
+              <div style={{width: '30%'}}>初当選</div>
+              <div style={{width: '70%'}}>{body.hatsu_tousen}</div>
+            </section>
+            <Divider />
+          </CardText>
+        </Card>
+      )
     })
     return (
       <MuiThemeProvider muiTheme={Mui}>
@@ -63,7 +81,7 @@ export default class Main extends Component {
           <AppBar
             title="Title"
             />
-          <div onClick={this.check}>state check</div>
+          <div onClick={this.check} style={{padding: '1rem'}}>state check</div>
           {list}
         </main>
       </MuiThemeProvider>
