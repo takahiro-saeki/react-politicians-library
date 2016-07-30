1869173814d1ee9f9578b9358df7b13f64f73189
takahiro-saeki <hiroooon69@gmail.com>
2016-07-30 11:47:11 +0900

    add move link. but I must check the state.type.

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import Main from './container/Main';
+import Result from './container/Result';
 import {Router, Route, browserHistory} from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
@@ -8,6 +9,7 @@ injectTapEventPlugin();
 render(
   <Router history={browserHistory}>
     <Route path='/' component={Main} />
+    <Route path='/:type' component={Result} />
   </Router>,
   document.getElementById('app')
 )
--- assets/js/container/Main.jsx
+++ assets/js/container/Main.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {browserHistory} from 'react-router';
 import request from 'superagent';
 import uuid from 'node-uuid';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -11,6 +12,7 @@ import url from '../data/url';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import listItems from '../data/listItems';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class Main extends Component {
   constructor(props) {
@@ -18,6 +20,7 @@ export default class Main extends Component {
     this.ajax()
     this.ajax = this.ajax.bind(this);
     this.typeChange = this.typeChange.bind(this);
+    this.location = this.location.bind(this);
     this.state ={
       body: [],
       count: 1,
@@ -46,6 +49,10 @@ export default class Main extends Component {
     this.setState(() => this.state.type = value)
   }
 
+  location() {
+    browserHistory.push(`/${this.state.type}`)
+  }
+
   render() {
     const selectItems = listItems.map(item => {
       return <MenuItem value={item.id} key={uuid.v4()} primaryText={item.type} />
@@ -59,6 +66,7 @@ export default class Main extends Component {
           <SelectField value={this.state.type} onChange={this.typeChange} maxHeight={200} fullWidth={true}>
             {selectItems}
           </SelectField>
+          <RaisedButton label="検索する" primary={true} fullWidth={true} style={{margin: '1rem auto'}} onClick={this.location}/>
         </section>
         </main>
       </MuiThemeProvider>
