6734a4e7e76d9f6b35e2cc0e5d9f8aeef57aeea2
takahiro-saeki <hiroooon69@gmail.com>
2016-07-31 17:14:26 +0900

    Work in progress...

--- assets/js/container/Main.jsx
+++ assets/js/container/Main.jsx
@@ -12,6 +12,7 @@ import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import listItems from '../data/listItems';
 import RaisedButton from 'material-ui/RaisedButton';
+import party from '../data/party';
 //後程削除
 const url = {
   req: 'http://seiji.kpi-net.com/api/',
@@ -46,26 +47,52 @@ export default class Main extends Component {
         const data = res.text.replace(/\r?\n/g,"").trim();
         const politicians = (new Function("return " + data))();
         console.log(politicians)
-        const jimin = [];
-        const koumei = [];
-        const dp = [];
-        const other = []
+        const other = [];
         politicians.map(poli => {
           switch(poli.seitou) {
-            case '自民党':
-              return jimin.push(poli)
-            case '公明党':
-              return koumei.push(poli)
-            case '民主党':
-              return dp.push(poli)
+            case party.jimin.name:
+              return party.jimin.data.push(poli)
+            case party.koumei.name:
+              return party.koumei.data.push(poli)
+            case party.dp.name:
+              return party.dp.data.push(poli)
+            case party.seikatsu.name:
+              return party.seikatsu.data.push(poli)
+            case party.independents.name:
+              return party.independents.data.push(poli)
+            case party.ishin.name:
+              return party.ishin.data.push(poli)
+            case party.everybody.name:
+              return party.everybody.data.push(poli)
+            case party.UnityParty.name:
+              return party.UnityParty.data.push(poli)
+            case party.green.name:
+              return party.green.data.push(poli)
+            case party.syamin.name:
+              return party.syamin.data.push(poli)
+            case party.kyousan.name:
+              return party.kyousan.data.push(poli)
+            case party.shintou.name:
+              return party.shintou.data.push(poli)
             default:
               return other.push(poli)
           }
         })
-        console.log(jimin)
-        console.log(koumei)
-        console.log(dp)
-        console.log(other)
+        console.log(party.jimin.data)
+        console.log(party.koumei.data)
+        console.log(party.dp.data)
+        console.log(party.seikatsu.data)
+        console.log(party.independents.data)
+        console.log(party.ishin.data)
+        console.log(party.everybody.data)
+        console.log(party.UnityParty.data)
+        console.log(party.green.data)
+        console.log(party.syamin.data)
+        console.log(party.kyousan.data)
+        console.log(party.shintou.data)
+        if(other.length !== 0) {
+          console.log(other)
+        }
       }
     })
   }
---
+++ assets/js/data/party.js
@@ -0,0 +1,52 @@
+const party = {
+  jimin: {
+    name: '自民党',
+    data: []
+  },
+  koumei: {
+    name: '公明党',
+    data: []
+  },
+  dp: {
+    name: '民主党',
+    data: []
+  },
+  seikatsu: {
+    name: '生活の党',
+    data: []
+  },
+  independents: {
+    name: '無所属',
+    data: []
+  },
+  ishin: {
+    name: '日本維新の会',
+    data: []
+  },
+  everybody: {
+    name: 'みんなの党',
+    data: []
+  },
+  UnityParty: {
+    name: '結いの党',
+    data: []
+  },
+  green: {
+    name: 'みどりの風',
+    data: []
+  },
+  syamin: {
+    name: '社民党',
+    data: []
+  },
+  kyousan: {
+    name: '共産党',
+    data: []
+  },
+  shintou: {
+    name: '新党大地',
+    data: []
+  }
+}
+
+export default party;
